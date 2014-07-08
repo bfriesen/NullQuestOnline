ff53c9a0e39319747af1ce3be91148e1bbd9d39b
Brian Friesen <brianfriesen@quickenloans.com>
2014-07-07 22:26:00 -0400

    Add rudimentary authentication.

---
+++ NullQuestOnline/Controllers/AccountController.cs
@@ -0,0 +1,33 @@
+﻿using System.Web.Mvc;
+using System.Web.Security;
+using NullQuestOnline.Models;
+
+namespace NullQuestOnline.Controllers
+{
+    public class AccountController : Controller
+    {
+        public ActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Login(Character character, string returnUrl)
+        {
+            FormsAuthentication.SetAuthCookie(character.Name, true);
+
+            if (returnUrl == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+
+            return Redirect(returnUrl);
+        }
+
+        public ActionResult Logout()
+        {
+            FormsAuthentication.SignOut();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
